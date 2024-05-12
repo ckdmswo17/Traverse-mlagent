bda0379dc8b72e443ccdede2d02a14a00e798c15
Han-eol <104002983+ahleno@users.noreply.github.com>
2024-05-12 21:08:33 +0900

    feat  : add map object
    화성행궁 오브젝트 추가

---
+++ Assets/Scripts/Tuho/ArrowCollisionHandler.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d79a5ee947046539bb159c69fd28929dcda823d129601f89edc16b21996c71af
+size 853
---
+++ Assets/Scripts/Tuho/GameController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7184da62c57127f7e6b8ae83da827bb845900708fa1ad96edfaff3dcecfcecc4
+size 668
